8c7599ff5e44d2e73d2333cd42236fe0ef73c2c2
secsandman <secsandman@gmail.com>
2018-03-09 15:30:53 -0600

    updated appspec with before, adter, start, added gradle wget and mkdir back to structure ec2 user data is not being copied from launch config.

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-sudo yum update -y
 sudo yum remove jre-1.7.0 -y
 sudo yum install java-1.8.0-openjdk-devel -y
+sudo rm /home/ec2-user/gradle-4.6-all.zip
+sudo rm -rf /opt/gradle/
+sudo mkdir /opt/gradle/
+export gradle_version=4.6
+cd /home/ec2-user/
+wget -c http://services.gradle.org/distributions/gradle-${gradle_version}-all.z$
 sudo chown -R ec2-user /opt/gradle/
-
-
+sudo unzip  gradle-${gradle_version}-all.zip -d /opt/gradle/
