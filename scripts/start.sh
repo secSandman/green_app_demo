06386f63ff5347ffaabf09469676ce9eef4601be
secsandman <secsandman@gmail.com>
2018-03-09 14:49:32 -0600

    approaching the finish line nohup jar /dev/null>&

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,4 +2,4 @@
 sudo chown ec2-user /home/ec2-user/green_app-0.1.0.jar
 search_terms='java'
 sudo kill $(ps aux | grep "$search_terms" | grep -v 'grep' | awk '{print $2}')
-sudo nohup java -jar /home/ec2-user/green_app-0.1.0.jar &
+sudo nohup java -jar /home/ec2-user/green_app-0.1.0.jar &>/dev/null &
