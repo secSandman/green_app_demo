1d68026fa662560b0c6e99762400fb1fc2e69773
secsandman <secsandman@gmail.com>
2018-03-09 17:34:07 -0600

    remove runas ec2-user

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,8 +2,8 @@
 sudo yum remove jre-1.7.0 -y
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo rm /home/ec2-user/gradle-4.6-all.zip
-sudo rm -rf /opt/gradle/
-sudo mkdir /opt/gradle/
+sudo rm -rf /opt/gradle
+sudo mkdir /opt/gradle
 export gradle_version=4.6
 cd /home/ec2-user/
 wget -c http://services.gradle.org/distributions/gradle-${gradle_version}-all.zip
